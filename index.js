8da96ff9f60b2d2b9dfc664e77ae060e00c2a01f
Saood Rana <79827270+saood-321-rana@users.noreply.github.com>
2023-08-23 11:58:54 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+let PersonName = " Muhammad Kamran";
+const Person_age = 30;
+var isStudent = true;
+let PersonSrNumber = 1;
+PersonName = " ALi raza";
+isStudent = false;
+console.log(Person_age + PersonSrNumber);
+console.log(isStudent);
---
+++ index.ts
@@ -0,0 +1,15 @@
+ 
+/*
+******variables********
+ let PersonName: string = " Muhammad Kamran";
+ const Person_age: number =  30;
+ var isStudent: boolean = true;
+ let PersonSrNumber: number = 1;
+
+
+PersonName = " ALi raza" ;
+isStudent = false;
+
+
+console.log(Person_age + PersonSrNumber);
+console.log(isStudent); */
